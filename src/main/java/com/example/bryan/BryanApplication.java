d9767adf6cb18d164599aaa7546753a483ec5ee8
Bryan <bryan.valarezo24@gmail.com>
2019-10-03 10:48:21 -0400

    Init the Spring Project

---
+++ src/main/java/com/example/bryan/BryanApplication.java
@@ -0,0 +1,13 @@
+package com.example.bryan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BryanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BryanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bryan/BryanApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.bryan;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BryanApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
